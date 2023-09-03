9d41736cc7c198c760b88272efa3da6121655087
crislarr <116907840+Crislarr@users.noreply.github.com>
2023-09-03 17:02:33 +0000

    Add complete css

--- main.js
+++ main.js
@@ -1,18 +1,126 @@
-function add(a, b) {
-  return a + b;
-}
+const $display = document.querySelector("#display");
+const $operators = document.querySelectorAll(".operator");
+const $numberButtons = document.querySelectorAll(".number");
+const $equalButton = document.querySelector("#equal-button");
+const $deleteButton = document.querySelector("#delete-button");
+const $acButton = document.querySelector("#ac-button");
+const $decimalButton = document.querySelector("#decimal-button");
+const $substractButton = document.querySelector("#substract");
 
-function substract(a, b) {
-  return a - b;
-}
+const operations = {
+  add: (a, b) => a + b,
+  substract: (a, b) => a - b,
+  multiply: (a, b) => a * b,
+  divide: (a, b) => a / b,
+};
 
-function multiply(a, b) {
-  return a * b;
-}
+let displayedNumber = [];
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 
-function divide(a, b) {
-  if (b === 0) {
-    return ":(";
+$numberButtons.forEach((button) =>
+  button.addEventListener("click", () => {
+    ConcatenateNumber(button.innerText);
+    actualizeDisplay();
+  })
+);
+
+$operators.forEach((currentOperator) =>
+  currentOperator.addEventListener("click", () => {
+    if (currentOperator.id === "substract" && !displayedNumber[0]) {
+      handleNegativeSign();
+      return;
+    }
+    if (!firstNumber) {
+      firstNumber = $display.innerText;
+      operator = currentOperator.id;
+      cleanDisplay();
+      enableDecimal();
+      return;
+    }
+    if (firstNumber) {
+      $equalButton.click();
+      operator = currentOperator.id;
+    }
+  })
+);
+
+$equalButton.addEventListener("click", () => {
+  secondNumber = $display.innerText;
+  if (!firstNumber || !secondNumber || !operator) {
+    return;
   }
-  return a / b;
+  let solution = parseFloat(
+    operate(firstNumber, operator, secondNumber).toFixed(4)
+  );
+
+  displayedNumber = Array.from(String(solution));
+
+  firstNumber = displayedNumber;
+
+  actualizeDisplay();
+
+  operator = "";
+
+  secondNumber = "";
+
+  enableDecimal();
+});
+
+$decimalButton.addEventListener("click", () => {
+  disableDecimal();
+});
+
+$acButton.addEventListener("click", () => {
+  resetCalculator();
+  actualizeDisplay();
+});
+
+$deleteButton.addEventListener("click", () => {
+  displayedNumber.pop();
+  actualizeDisplay();
+});
+
+function operate(a, operator, b) {
+  return operations[`${operator}`](Number(a), Number(b));
+}
+
+function ConcatenateNumber(number) {
+  displayedNumber.push(number);
+}
+
+function actualizeDisplay() {
+  $display.innerText = displayedNumber.join("");
+}
+
+function disableDecimal() {
+  $decimalButton.disabled = true;
+}
+
+function enableDecimal() {
+  $decimalButton.disabled = false;
+}
+
+function resetCalculator() {
+  displayedNumber = [];
+
+  firstNumber = "";
+
+  secondNumber = "";
+
+  operator = "";
+}
+
+function handleNegativeSign() {
+  if ($display.innerText === "-") {
+    return;
+  }
+  displayedNumber.push("-");
+  actualizeDisplay();
+  return;
+}
+
+function cleanDisplay() {
+  displayedNumber = [];
 }
